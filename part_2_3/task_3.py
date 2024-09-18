88a3ce63f074ae6fbc22ee20fac8da9944cbbe0b
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-09-18 20:18:54 +0300

    fixed comments

--- part_2_3/task_3.py
+++ part_2_3/task_3.py
@@ -1,7 +1,4 @@
 #На вход программе подаются четыре строки (каждая на отдельной строке): строка-разделитель и три строки.
 #Программа должна вывести введённые три строки через разделитель.
 separator = input()
-second_raw = input()
-third_raw = input()
-fourth_raw = input()
-print(second_raw, third_raw, fourth_raw, sep=separator)
+print(input(), input(), input(), sep = separator)
--- part_2_4/task_2.py
+++ part_2_4/task_2.py
@@ -1,6 +1,3 @@
 #На вход программе подаются три целых числа, каждое на отдельной строке.
 #Программа должна вывести сумму введённых чисел.
-first_number = int(input())
-second_number = int(input())
-third_number = int(input())
-print(first_number + second_number + third_number)
+print(int(input()) + int(input()) + int(input()))
