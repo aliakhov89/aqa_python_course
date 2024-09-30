0ff6a86cc90847fd7e43b260f1069bc9452a7bbe
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-09-30 19:29:33 +0300

    fixed comments in 6_3

--- part_6_2/task_5.py
+++ part_6_2/task_5.py
@@ -2,13 +2,9 @@
 # можно ли из длин этих строк построить арифметическую прогрессию.
 #Программа должна вывести строку «YES», если из длин введенных слов можно построить арифметическую прогрессию, или «NO» в противном случае.
 
-typed_text_1 = input()
-typed_text_2 = input()
-typed_text_3 = input()
+typed_text_1, typed_text_2, typed_text_3 = input(), input(), input()
+min_length, med_length, max_length = sorted([len(typed_text_1), len(typed_text_2), len(typed_text_3)])
 
-max_length = max(len(typed_text_1), len(typed_text_2), len(typed_text_3))
-min_length = min(len(typed_text_1), len(typed_text_2), len(typed_text_3))
-med_length = len(typed_text_1) + len(typed_text_2) + len(typed_text_3) - max_length - min_length
 
 if med_length - min_length == max_length - med_length:
     print('YES')
