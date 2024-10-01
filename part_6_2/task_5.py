1866edf6163c3670c0425af9d2c021f58e865df6
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-10-01 17:27:53 +0300

    made changes in task_1 and task_5

--- part_6_2/task_5.py
+++ part_6_2/task_5.py
@@ -3,9 +3,14 @@
 #Программа должна вывести строку «YES», если из длин введенных слов можно построить арифметическую прогрессию, или «NO» в противном случае.
 
 typed_text_1, typed_text_2, typed_text_3 = input(), input(), input()
-min_length, med_length, max_length = sorted([len(typed_text_1), len(typed_text_2), len(typed_text_3)])
 
+typed_text_1_length = len(typed_text_1)
+typed_text_2_length = len(typed_text_2)
+typed_text_3_length = len(typed_text_3)
 
+max_length = max(typed_text_1_length, typed_text_2_length, typed_text_3_length)
+min_length = min(typed_text_1_length, typed_text_2_length, typed_text_3_length)
+med_length = typed_text_1_length + typed_text_2_length + typed_text_3_length - max_length - min_length
 if med_length - min_length == max_length - med_length:
     print('YES')
 else:
