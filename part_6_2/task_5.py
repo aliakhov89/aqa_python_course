eef62c6bdb295fdd18e8a29ced14845f10a5486a
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-10-02 13:05:39 +0300

    made changes in task_5

--- part_6_2/task_5.py
+++ part_6_2/task_5.py
@@ -2,11 +2,7 @@
 # можно ли из длин этих строк построить арифметическую прогрессию.
 #Программа должна вывести строку «YES», если из длин введенных слов можно построить арифметическую прогрессию, или «NO» в противном случае.
 
-typed_text_1, typed_text_2, typed_text_3 = input(), input(), input()
-
-typed_text_1_length = len(typed_text_1)
-typed_text_2_length = len(typed_text_2)
-typed_text_3_length = len(typed_text_3)
+typed_text_1_length, typed_text_2_length, typed_text_3_length = len(input()), len(input()), len(input())
 
 max_length = max(typed_text_1_length, typed_text_2_length, typed_text_3_length)
 min_length = min(typed_text_1_length, typed_text_2_length, typed_text_3_length)
