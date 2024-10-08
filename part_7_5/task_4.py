900bc0e78a1f9c81b9b2e8fad87fb5fa05a1e45f
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-10-08 23:56:59 +0300

    fix comment task 4 in part 7_5

--- part_7_5/task_4.py
+++ part_7_5/task_4.py
@@ -9,7 +9,7 @@
 
 
 num = int(input())
-counter = 0
+nmbs_sum = 0
 nmbs_amount = 0
 nmbs_product = 1
 get_last_digit = num % 10
@@ -17,10 +17,10 @@ while num != 0:
     last_digit = num % 10
     nmbs_amount += last_digit
     nmbs_product *= last_digit
-    counter += 1
+    nmbs_sum += 1
     first_digit = num
     num = num // 10
-arithmetic_mean_of_nmbs = nmbs_amount / counter
-print(nmbs_amount, counter, nmbs_product, arithmetic_mean_of_nmbs, first_digit, first_digit + get_last_digit, sep = '\n')
+arithmetic_mean_of_nmbs = nmbs_amount / nmbs_sum
+print(nmbs_amount, nmbs_sum, nmbs_product, arithmetic_mean_of_nmbs, first_digit, first_digit + get_last_digit, sep = '\n')
 
 
