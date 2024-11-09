72d956a44a501f581c046cfa1923b418c3c37eef
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-11-09 08:43:26 +0200

    fix one comment in task 3 part 11_6

--- part_11_6/task_3.py
+++ part_11_6/task_3.py
@@ -10,3 +10,14 @@ for word in typed_text:
     if word.lower() == 'a' or word.lower() == 'an' or word.lower() == 'the':
         counter += 1
 print("Total amount of articles:", counter)
+
+
+#articles in th list:
+
+typed_text = input().split()
+articles = ['a', 'an', 'the']
+counter = 0
+for word in typed_text:
+    if word.lower() in articles:
+        counter += 1
+print("Total amount of articles:", counter)
