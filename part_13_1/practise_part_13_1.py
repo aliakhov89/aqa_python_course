f194cc874c702396d317a9213dec93cabfde3721
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-11-12 20:10:13 +0200

    tasks for part_13_1

---
+++ part_13_1/practise_part_13_1.py
@@ -0,0 +1,18 @@
+def draw_box():
+    for _ in range(5):
+        print('*' * 7)
+
+draw_box()
+print()
+draw_box()
+print()
+draw_box()
+
+
+
+def print_message():
+    print('Я - Артур,')
+    print('король британцев. ')
+
+
+print_message()
