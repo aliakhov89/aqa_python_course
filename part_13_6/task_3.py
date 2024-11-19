723b22ffbef7a357a0fc76b0e758b73761bca414
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-11-19 15:34:46 +0200

    Tasks for part 13.6

---
+++ part_13_6/task_3.py
@@ -0,0 +1,21 @@
+#Напишите функцию solve(a, b, c), которая принимает в качестве аргументов три целых числа a, b, c – коэффициенты квадратного уравнения
+# ax**2+bx+c=0 и возвращает его корни в порядке возрастания.
+
+#Примечание Гарантируется, что квадратное уравнение имеет хотя бы один корень.
+from math import *
+def solve(a, b, c):
+    discriminant = pow(b, 2) - 4 * a * c
+    x1 = (-b + sqrt(discriminant)) / (2 * a)
+    x2 = (-b - sqrt(discriminant)) / (2 * a)
+    if discriminant == 0:
+        return x1, x2
+    else:
+        if x1 >= x2:
+            return x2, x1
+        else:
+            return x1, x2
+
+a, b, c = int(input()), int(input()), int(input())
+
+x1, x2 = solve(a, b, c)
+print(x1, x2)
