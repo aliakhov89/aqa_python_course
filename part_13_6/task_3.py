12b7e17c3f1bed028043211717d10101215056c7
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-11-20 15:24:56 +0200

    fix comment in task 3  part 13.6

--- part_13_6/task_3.py
+++ part_13_6/task_3.py
@@ -7,13 +7,7 @@ def solve(a, b, c):
     discriminant = pow(b, 2) - 4 * a * c
     x1 = (-b + sqrt(discriminant)) / (2 * a)
     x2 = (-b - sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return x1, x2
-    else:
-        if x1 >= x2:
-            return x2, x1
-        else:
-            return x1, x2
+    return sorted([x1,x2])
 
 a, b, c = int(input()), int(input()), int(input())
 
