ce277aded25c6fdf9b2bdf1b97c8a2241896e878
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-11-27 18:10:28 +0200

    fixed comment

--- part_14_exam/task_5.py
+++ part_14_exam/task_5.py
@@ -2,17 +2,20 @@
 # которая принимает на вход два аргумента language – язык ru или en
 # и number – номер месяца (от 1 до 12) и возвращает название месяца на русском или английском языке.
 
-months_ru = { 1 : 'январь', 2 : 'февраль', 3 : 'март', 4 : 'апрель', 5 : 'май', 6 : 'июнь', 7 : 'июль', 8 : 'август', 9 : 'сентябрь', 10 : 'октябрь', 11 : 'ноябрь', 12 : 'декабрь'}
-months_en = { 1 : 'january', 2 : 'february', 3 : 'march', 4 : 'april', 5 : 'may', 6 : 'june', 7 : 'july', 8 : 'august', 9 : 'september', 10 : 'october', 11 : 'november', 12 : 'december'}
+MONTHS_RU = {1 : 'январь', 2 : 'февраль', 3 : 'март', 4 : 'апрель', 5 : 'май', 6 : 'июнь', 7 : 'июль', 8 : 'август', 9 : 'сентябрь', 10 : 'октябрь', 11 : 'ноябрь', 12 : 'декабрь'}
+MONTHS_EN = {1 : 'january', 2 : 'february', 3 : 'march', 4 : 'april', 5 : 'may', 6 : 'june', 7 : 'july', 8 : 'august', 9 : 'september', 10 : 'october', 11 : 'november', 12 : 'december'}
 
 def get_month(language, number):
     if language == 'ru':
-        return months_ru[number]
+        return MONTHS_RU[number]
     else:
-        return months_en[number]
+        return MONTHS_EN[number]
 
 lan = input()
 num = int(input())
 
 
 print(get_month(lan, num))
+
+
+
