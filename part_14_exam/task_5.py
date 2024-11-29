4b1764548fd934937168b1192542858c37c6e959
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-11-29 17:01:41 +0200

    fix comment in part 14 task 5

--- part_14_exam/task_5.py
+++ part_14_exam/task_5.py
@@ -5,17 +5,17 @@
 MONTHS_RU = {1 : 'январь', 2 : 'февраль', 3 : 'март', 4 : 'апрель', 5 : 'май', 6 : 'июнь', 7 : 'июль', 8 : 'август', 9 : 'сентябрь', 10 : 'октябрь', 11 : 'ноябрь', 12 : 'декабрь'}
 MONTHS_EN = {1 : 'january', 2 : 'february', 3 : 'march', 4 : 'april', 5 : 'may', 6 : 'june', 7 : 'july', 8 : 'august', 9 : 'september', 10 : 'october', 11 : 'november', 12 : 'december'}
 
-def get_month(language, number):
+def get_month(language, number_ru, number_en):
     if language == 'ru':
-        return MONTHS_RU[number]
+        return MONTHS_RU[number_ru]
     else:
-        return MONTHS_EN[number]
+        return MONTHS_EN[number_en]
 
 lan = input()
 num = int(input())
 
 
-print(get_month(lan, num))
+print(get_month(lan, num, num))
 
 
 
